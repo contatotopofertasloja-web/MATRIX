8b13649df8ef5d626d838adf0f9c8e19e3927eb9
Vandeir Scheffelt <contato.topofertas.loja@gmail.com>
2025-09-03 20:44:13 -0300

    fix: shim em UTF-8 (configs/src/core/settings.js)

---
+++ configs/src/core/settings.js
@@ -0,0 +1,4 @@
+﻿// Shim p/ compat: redireciona 'configs/src/core/settings.js' p/ o módulo real
+export * from '../../../src/core/settings.js';
+export { default } from '../../../src/core/settings.js';
+
